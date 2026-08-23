0fdd89a78c87a4aa4ccb2ec451440c76fda1b1bd
Your Name <2174064279@qq.com>
2026-08-23 17:27:04 +0800

    2

--- src/site.config.ts
+++ src/site.config.ts
@@ -62,7 +62,8 @@ export const theme: ThemeUserConfig = {
           { title: 'Tech', link: '/tech' },
           { title: 'Daily', link: '/daily' },
           { title: 'Monthly', link: '/monthly' },
-          { title: 'Notes', link: 'https://notes.jiaxin404.top/' }
+          { title: 'Notes', link: 'https://notes.jiaxin404.top/' },
+          { title: 'Feishu', link: 'https://kcn04fsleza4.feishu.cn/wiki/CZdpwGVZiiv9JQknRa6cSHkjnUf' }
         ]
       },
       { title: 'Projects', link: '/projects' },
